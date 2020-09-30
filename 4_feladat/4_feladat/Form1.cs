d500f14ec93679205a4d50895939a93518258e7a
Quang Anh Nguyen <quang.nguyen@stud.uni-corvinus.hu>
2020-09-30 19:31:33 +0200

    Edmx

--- 4_feladat/4_feladat/Form1.cs
+++ 4_feladat/4_feladat/Form1.cs
@@ -8,13 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace _4_feladat
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            Loaddata();
+        }
+        private void Loaddata()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
