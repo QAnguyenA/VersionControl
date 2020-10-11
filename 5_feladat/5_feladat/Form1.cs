733338ac50055e575ed425872616cc5c3c397abf
Quang Anh Nguyen <quang.nguyen@stud.uni-corvinus.hu>
2020-10-11 13:13:12 +0200

    5. Portfólió tárolás

--- 5_feladat/5_feladat/Form1.cs
+++ 5_feladat/5_feladat/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _5_feladat.Enitities;
 
 namespace _5_feladat
 {
@@ -14,11 +15,21 @@ namespace _5_feladat
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
