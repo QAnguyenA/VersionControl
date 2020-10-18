8a63c3f1802ea94ad88d06fa1cf9bc4d5cf22e3b
Quang Anh Nguyen <quang.nguyen@stud.uni-corvinus.hu>
2020-10-18 12:46:10 +0200

    3.Webhívás

--- 6_feladat/6_feladat/Form1.cs
+++ 6_feladat/6_feladat/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _6_feladat.MnbServiceReference;
 
 namespace _6_feladat
 {
@@ -15,6 +17,34 @@ namespace _6_feladat
         public Form1()
         {
             InitializeComponent();
+            hivas();
+        }
+
+        private void hivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw= new StreamWriter(sfd.FileName,false, Encoding.UTF8))
+            {
+                foreach (var a in result)
+                {
+                    sw.Write(a);
+                }
+            }
+
         }
     }
 }
