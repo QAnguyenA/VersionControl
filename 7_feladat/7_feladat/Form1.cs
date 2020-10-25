aa769ecf39605e3caad61584b792a9ace56634b2
Quang Anh Nguyen <quang.nguyen@stud.uni-corvinus.hu>
2020-10-25 20:32:25 +0100

    8. Félkész felhasználői felület

--- 7_feladat/7_feladat/Form1.cs
+++ 7_feladat/7_feladat/Form1.cs
@@ -17,25 +17,65 @@ namespace _7_feladat
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        List<int> Férfi;
+        List<int> Nő;
 
         Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
 
-            Population = GetPopulation(@"C:\Temp\nép.csv");
+            
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-            Simulation();
+            
 
 
+            
+
+
+        }
+
+        private void SimStep(int year, Person person)
+        {
+
+            if (!person.IsAlive) return;
+
+
+            byte age = (byte)(year - person.BirthYear);
+
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.Probability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.Probability).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NumberOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
 
         private void Simulation()
         {
            
-            for (int year = 2005; year <= 2024; year++)
+            for (int year = 2005; year <= numericUpDown1.Value; year++)
             {
                 
 
@@ -52,10 +92,17 @@ namespace _7_feladat
                 int nbrOfFemales = (from x in Population
                                     where x.Gender == Gender.Female && x.IsAlive
                                     select x).Count();
+
                 Console.WriteLine(
-                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+                string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+
             }
 
+        }
+
+        private void DisplayResults()
+        {
+            richTextBox1.Text = Férfi.ToString();
         }
 
     
@@ -122,41 +169,18 @@ namespace _7_feladat
             return deathProbabilities;
         }
 
-
-        public void SimStep(int year, Person person)
+        private void button2_Click(object sender, EventArgs e)
         {
-           
-            if (!person.IsAlive) return;
-
-            
-            byte age = (byte)(year - person.BirthYear);
-
-            
-            double pDeath = (from x in DeathProbabilities
-                             where x.Gender == person.Gender && x.Age == age
-                             select x.Probability).FirstOrDefault();
-           
-            if (rng.NextDouble() <= pDeath)
-                person.IsAlive = false;
-
-            
-            if (person.IsAlive && person.Gender == Gender.Female)
-            {
-               
-                double pBirth = (from x in BirthProbabilities
-                                 where x.Age == age
-                                 select x.Probability).FirstOrDefault();
-                
-                if (rng.NextDouble() <= pBirth)
-                {
-                    Person újszülött = new Person();
-                    újszülött.BirthYear = year;
-                    újszülött.NumberOfChildren = 0;
-                    újszülött.Gender = (Gender)(rng.Next(1, 3));
-                    Population.Add(újszülött);
-                }
-            }
+            Simulation();
+            dis();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            textBox1.Text = ofd.FileName;
+            Population = GetPopulation(textBox1.Text);
+        }
     }
 }
