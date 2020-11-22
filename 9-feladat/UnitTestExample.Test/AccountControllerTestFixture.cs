d46c6ca7c1602404cc79bcd17b0a4bcb28e218a5
Quang Anh Nguyen <quang.nguyen@stud.uni-corvinus.hu>
2020-11-22 21:12:16 +0100

    7.rész

--- 9-feladat/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ 9-feladat/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -11,20 +11,22 @@ namespace UnitTestExample.Test
     public class AccountControllerTestFixture
     {
         [
-            Test,
-            TestCase("abcd1234", false),
-            TestCase("irf@uni-corvinus", false),
-            TestCase("irf.uni-corvinus.hu", false),
-            TestCase("irf@uni-corvinus.hu", true)
+            
+            TestCase("ABCD1234",false),
+            TestCase("Ab1234",false),
+            TestCase("Abcd1234",true),
+            TestCase("abcd1234",false),
+            TestCase("abcdABCD",false)
 
         ]
-        public void TestValidateEmail(string email, bool expectedResult)
+
+        public void TestValidatePassword(string password, bool expectedResult)
         {
             // Arrange
             var accountController = new AccountController();
 
             // Act
-            var actualResult = accountController.ValidateEmail(email);
+            var actualResult = accountController.ValidateEmail(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
